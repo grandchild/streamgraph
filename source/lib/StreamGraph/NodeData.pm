84f8106b83a4f4d1fbb3f55d022f97236a0c158f
jakob <grandchild@gmx.net>
2016-06-21 21:50:17 +0200

    Add defaults for all NodeData fields

--- source/lib/StreamGraph/NodeData.pm
+++ source/lib/StreamGraph/NodeData.pm
@@ -11,22 +11,22 @@ use strict;
 has initCode            => ( is=>"rw", default=>"" );
 has workCode            => ( is=>"rw", default=>"" );
 has globalVariables     => ( is=>"rw", default=>"" );
-has timesPush           => ( is=>"rw", default=>"" );
-has timesPop            => ( is=>"rw", default=>"" );
-has timesPeek           => ( is=>"rw", default=>"" );
+has timesPush           => ( is=>"rw", default=>0 );
+has timesPop            => ( is=>"rw", default=>0 );
+has timesPeek           => ( is=>"rw", default=>0 );
 
-has joinType            => ( is=>"rw" );
-has joinMultiplicities  => ( is=>"rw" );
-has joinRRForAll        => ( is=>"rw" );
+has joinType            => ( is=>"rw", default=>"rr" );
+has joinMultiplicities  => ( is=>"rw", default=>(0) );
+has joinRRForAll        => ( is=>"rw", default=>1 );
 
-has splitType           => ( is=>"rw" );
-has splitMultiplicities => ( is=>"rw" );
-has splitRRForAll       => ( is=>"rw" );
+has splitType           => ( is=>"rw", default=>"duplicate" );
+has splitMultiplicities => ( is=>"rw", default=>(0) );
+has splitRRForAll       => ( is=>"rw", default=>1 );
 
-has inputType           => ( is=>"rw" );
-has inputCount          => ( is=>"rw" );
+has inputType           => ( is=>"rw", default=>"void" );
+has inputCount          => ( is=>"rw", default=>0 );
 
-has outputType          => ( is=>"rw" );
-has outputCount         => ( is=>"rw" );
+has outputType          => ( is=>"rw", default=>"void" );
+has outputCount         => ( is=>"rw", default=>0 );
 
 1;
