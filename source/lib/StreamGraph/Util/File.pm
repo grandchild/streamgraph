a0243c3a950f25573232669bca55652d8278fa8e
jakob <grandchild@gmx.net>
2016-06-27 11:16:29 +0200

    Add more file write functions

--- source/lib/StreamGraph/Util/File.pm
+++ source/lib/StreamGraph/Util/File.pm
@@ -2,19 +2,41 @@ package StreamGraph::Util::File;
 
 use strict;
 
-# gets string to write and name of file
-sub writeToFile {
+
+sub _write {
 	my $writeString = shift;
 	my $fileName = shift;
 	if(!defined($fileName) || $fileName eq ""){
 		$fileName = "a";
 	}
-	if(!(substr($fileName, -4, 4) eq ".str")){
-		$fileName .= ".str";
-	}
 	open(my $fh, '>', $fileName);
 	print $fh $writeString;
 	close $fh;
+}
+
+# $file->writeFile($string, "filename.ext");
+sub writeFile {
+	_write(@_);
+}
+
+# $file->writeStreamitSource($string, "filename");
+sub writeStreamitSource {
+	my $writeString = shift;
+	my $fileName = shift;
+	if(!(substr($fileName, -4, 4) eq ".str")){
+		$fileName .= ".str";
+	}
+	_write($writeString, $fileName);
+}
+
+# $file->writeConfig($string, "filename");
+sub writeConfig {
+	my $writeString = shift;
+	my $fileName = shift;
+	if(!(substr($fileName, -4, 4) eq ".conf")){
+		$fileName .= ".conf";
+	}
+	_write($writeString, $fileName);
 }
 
 1;
