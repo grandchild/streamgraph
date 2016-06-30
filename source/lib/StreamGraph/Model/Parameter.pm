730321914481a9dc4e56ba75cf1f47a4208feaa3
jakob <grandchild@gmx.net>
2016-06-30 13:08:43 +0200

    Add value field to Model::Parameter

--- source/lib/StreamGraph/Model/Parameter.pm
+++ source/lib/StreamGraph/Model/Parameter.pm
@@ -7,6 +7,7 @@ use Moo;
 extends "StreamGraph::Model::Node";
 
 has outputType  => ( is=>"rw", default=>"void" );
+has value       => ( is=>"rw", default=>0);
 
 has saveMembers => (is=>"ro", default=>sub{[qw(name x y outputType)]});
 
