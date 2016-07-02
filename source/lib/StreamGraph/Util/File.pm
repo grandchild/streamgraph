3c3867ab432b0abd7d4a41f4a977590d164d7a87
jakob <grandchild@gmx.net>
2016-07-02 17:39:22 +0200

    Add file load & save
    And remove unused writeConfig() function

--- source/lib/StreamGraph/Util/File.pm
+++ source/lib/StreamGraph/Util/File.pm
@@ -1,6 +1,11 @@
 package StreamGraph::Util::File;
 
 use strict;
+use warnings;
+use YAML qw(LoadFile DumpFile Load Dump Bless Blessed);
+use Data::Dump qw(dump);
+
+use StreamGraph::Model::NodeFactory;
 
 
 sub _write {
@@ -27,14 +32,40 @@ sub writeStreamitSource {
 	_write($writeString, $filename);
 }
 
-# $file->writeConfig($string, "filename");
-sub writeConfig {
-	my $writeString = shift;
-	my $fileName = shift;
-	if(!(substr($fileName, -4, 4) eq ".conf")){
-		$fileName .= ".conf";
-	}
-	_write($writeString, $fileName);
+# Util::File::save($filename, $graph);
+sub save {
+	my ($filename, $graph) = @_;
+	my @nodes =
+		map {
+			# print $_->{data} . "\n";
+			{ type=>ref($_->{data}), data=>$_->{data} }
+		} $graph->get_items;
+	my @connections =
+		map {
+			# print $_->[0]->{data} . " -----> " . $_->[1]->{data} . "\n";
+			{ from=>$_->[0]->{data}->id, to=>$_->[1]->{data}->id }
+		} $graph->get_connections;
+	my $savestruct = { nodes=>\@nodes, connections=>\@connections };
+	Bless($savestruct)->keys(["nodes", "connections"]);  # force this key ordering
+	DumpFile($filename, ("Streamit Graph File", $savestruct));
+}
+
+# Util::File::load($filename)
+sub load {
+	my ($filename) = @_;
+	my $nodeFactory = StreamGraph::Model::NodeFactory->new;
+	my $obj = LoadFile($filename);
+	my @nodes =
+		map {
+			my $type = $_->{type};
+			my $parameters = $_->{data};
+			$nodeFactory->createNode(type=>$type, %{$parameters});
+		} @{$obj->{nodes}};
+	my @connections =
+		map {
+			( $_->{from}, $_->{to} )
+		} @{$obj->{connections}};
+	return (\@nodes, \@{$obj->{connections}});
 }
 
 1;
