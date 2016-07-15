67573926f1ae5a7bd13d12848d53eecd7e955083
jakob <grandchild@gmx.net>
2016-07-15 12:23:20 +0200

    En-/disable I/O pins only on filters

--- source/lib/StreamGraph/View/Item.pm
+++ source/lib/StreamGraph/View/Item.pm
@@ -161,8 +161,10 @@ sub update {
 
 	$self->{border}->update();
 	$self->set(width=>$self->{border}->get('width'), height=>$self->{border}->get('height'));
-	$self->{hotspots}{'toggle_left'}->set(enabled=>$self->{data}->inputType ne "void");
-	$self->{hotspots}{'toggle_right'}->set(enabled=>$self->{data}->outputType ne "void");
+	if ($self->isFilter) {
+		$self->{hotspots}{'toggle_left'}->set(enabled=>$self->{data}->inputType ne "void");
+		$self->{hotspots}{'toggle_right'}->set(enabled=>$self->{data}->outputType ne "void");
+	}
 }
 
 # $item->add_hotspot
