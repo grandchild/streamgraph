f0718acaa5fe6997554e0aa2399305866ff8e733
jakob <grandchild@gmx.net>
2016-09-14 13:06:08 +0200

    Doc: Remove title page from pod2texi output

--- doc/pod/mkpodmod.sh
+++ doc/pod/mkpodmod.sh
@@ -7,7 +7,9 @@ modpath="$dir/../../source/lib/StreamGraph/$1.pm"
 if [ -e "$modpath" ]; then
 	cd $dir
 	mkdir -p $dir/$subdir
-	pod2texi "$modpath" > "$1.texi" || echo "Error creating TexInfo"
+	pod2texi --unnumbered-sections "$modpath" > "$1.texi" || echo "Error creating TexInfo"
+	sed -i"" '/@node Top/d' "$1.texi"
+	sed -i"" '/@top StreamGraph::.*/d' "$1.texi"
 	texi2pdf "$1.texi" -o "$1.pdf" &> /dev/null || echo "Error creating PDF"
 else
 	echo No module $modpath!
