9b19856f0743f74656f99f6b46d16647db5b0b61
jakob <grandchild@gmx.net>
2016-09-14 18:33:34 +0200

    Doc: Fix path bug in mkdocs script

--- doc/mkdocs.sh
+++ doc/mkdocs.sh
@@ -1,8 +1,10 @@
+dir=$(realpath $(dirname "$0"))
+
 echo ::: Generating Installation LaTeX PDF
-pandoc -f markdown -V geometry="margin=2cm" -V colorlinks -t latex --latex-engine=xelatex -o Install.pdf Install.md
+pandoc -f markdown -V geometry="margin=2cm" -V colorlinks -t latex --latex-engine=xelatex -o $dir/Install.pdf $dir/Install.md
 
 echo ::: Generating Perl module documentation
-pod/mkpodmod.sh
+$dir/pod/mkpodmod.sh
 
 echo ::: Generating streamgraph.pl documentation
 echo TODO
