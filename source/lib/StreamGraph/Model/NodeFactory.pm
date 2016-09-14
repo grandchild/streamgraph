95b097f2522275c8576a7b011a081f10ab402765
jakob <grandchild@gmx.net>
2016-09-14 12:23:14 +0200

    Doc Model::NodeFactory

--- source/lib/StreamGraph/Model/NodeFactory.pm
+++ source/lib/StreamGraph/Model/NodeFactory.pm
@@ -56,3 +56,52 @@ sub createVoidEnd {
 }
 
 1;
+__END__
+
+=head1 StreamGraph::Model::NodeFactory
+
+Create general Nodes and some standardized nodes for StreamGraph::GraphCompat.
+
+=head2 Properties
+
+None.
+
+=head2 Methods
+
+=over
+
+=item C<StreamGraph::Model::NodeFactory-E<gt>new()>
+
+Create a StreamGraph::Model::NodeFactory.
+
+=item C<createNode(@attributes)>
+
+C<return> a new node of type C<$attributes{"type"}>.
+
+Takes a hash of C<%attributes> to pass to the new node.
+
+
+=item C<createIdentity($datatype)>
+
+C<return> a new node of type StreamGraph::Model::Filter. 
+
+The created filter will have the work section of
+
+	push(pop());
+
+and so will pass on all data packets unchanged.
+
+This function is used by StreamGraph::GraphCompat.
+
+
+=item C<createVoidEnd($type, $count)>
+
+C<return> a new node of type StreamGraph::Model::Filter or 0 on error if the
+C<$type> is invalid.
+
+C<$type> is a string and can either be C<"source"> or C<"sink">.
+
+C<$count> determines how many outputs (source) or inputs (sink) the node will
+have.
+
+=back
