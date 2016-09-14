61d8f49c8de0ad8b223d31ced4482882b3f4ed1a
jakob <grandchild@gmx.net>
2016-09-14 14:42:33 +0200

    Doc: Model::Saveable

--- source/lib/StreamGraph/Model/Saveable.pm
+++ source/lib/StreamGraph/Model/Saveable.pm
@@ -18,3 +18,35 @@ sub yaml_dump {
 }
 
 1;
+__END__
+
+=head1 StreamGraph::Model::Saveable
+
+Base class for objects that should be serialized when saving the graph.
+
+=head2 Properties
+
+=over
+
+=item C<saveMembers> (list[String])
+
+A string list of members that should be saved.
+
+=back
+
+=head2 Methods
+
+=over
+
+=item C<StreamGraph::Model::Saveable-E<gt>new()>
+
+Create a StreamGraph::Model::Saveable.
+
+=item C<yaml_dump()>
+
+C<return> a blessed YAML wrapped version of C<$self> with knowledge of members
+to be saved and their order.
+
+See documentation of YAML module for details.
+
+=back
