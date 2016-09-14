cabd54c146b18cafef427d6f47040bd3ae352511
jakob <grandchild@gmx.net>
2016-09-14 20:04:13 +0200

    Doc: Fancy up mkpodmod script and skip some View folders

--- doc/mkdocs.sh
+++ doc/mkdocs.sh
@@ -5,7 +5,7 @@ echo ::: Generating Installation LaTeX PDF
 pandoc -f markdown -V geometry="margin=2cm" -V colorlinks -t latex --latex-engine=xelatex -o Install.pdf Install.md
 
 if [[ "$1" != "nopods" ]]; then
-echo ::: Generating Perl module documentation
+echo ::: Generating Perl module documentation \(pass \'nopods\' to skip\)
 pod/mkpodmod.sh
 fi
 
@@ -16,9 +16,7 @@ sed -i"" '/@node Top/d' "$outfilename.texi"
 sed -i"" '/@top streamgraph.pl/d' "$outfilename.texi"
 texi2pdf -b "$outfilename.texi" -o "$outfilename.pdf" &> /dev/null || echo "Error creating PDF"
 rm $outfilename.{aux,toc,log,texi}
-echo $outfilename.pdf
 
 echo ::: Generating main Documentation PDF
 pdflatex -synctex=1 -interaction=nonstopmode Documentation.tex &> /dev/null || echo "Error creating PDF"
 pdflatex -synctex=1 -interaction=nonstopmode Documentation.tex &> /dev/null || echo "Error creating PDF"
-echo Documentation.pdf
--- doc/pod/mkpodmod.sh
+++ doc/pod/mkpodmod.sh
@@ -1,26 +1,33 @@
 
 dir=$(realpath $(dirname "$0"))
-sourcepath="$dir/../../source/lib/"
+cd $dir
+sourcepath="../../source/lib/"
 
 if [[ $# -eq 0 ]]; then
-	for pm in $(find $sourcepath -name "*.pm") ; do
+	filecmd="find $sourcepath -name *.pm -not ( -path *View/Border* -o -path *View/Content* -o -path *View/HotSpot* )"
+	total=$($filecmd | wc -l)
+	echo -n [ ;	printf '%0.s ' $(seq 1 $total) ; echo -en "] $total\r" ; echo -en "["
+	for pm in $($filecmd) ; do
 		pm=${pm#$sourcepath}
 		pm=${pm%.pm}
-		$dir/mkpodmod.sh $pm
+		./mkpodmod.sh $pm
+		echo -n =
 	done
+	echo -en "\r"
+	printf '%0.s ' $(seq 0 $total)
+	echo -en "      \r"
 else
 	subdir=$(dirname $1)
-	docdir="$dir/$subdir"
+	docdir="$subdir"
 	modpath="$sourcepath/$1.pm"
 
 	if [ -e "$modpath" ]; then
-		cd $dir
-		mkdir -p $dir/$subdir
-		pod2texi --unnumbered-sections "$modpath" > "$1.texi" || echo "Error creating TexInfo"
+		mkdir -p $subdir
+		pod2texi --unnumbered-sections "$modpath" > "$1.texi" || echo "Error creating TexInfo $1.texi"
 		sed -i"" '/@node Top/d' "$1.texi"
 		sed -i"" '/@top StreamGraph::.*/d' "$1.texi"
-		texi2pdf -b "$1.texi" -o "$1.pdf" &> /dev/null || echo "Error creating PDF"
-		echo "$1.pdf"
+		texi2pdf -b "$1.texi" -o "$1.pdf" &> /dev/null || echo "Error creating PDF $1.pdf"
+		# echo "$1.pdf"
 	else
 		echo No module $modpath!
 	fi
