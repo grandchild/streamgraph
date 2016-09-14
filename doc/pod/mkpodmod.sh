4342a50b01672b763fdb7b1790d9e450b2d1f30c
jakob <grandchild@gmx.net>
2016-09-14 16:17:13 +0200

    Update mkpodmod to build all modules when no args

--- doc/pod/mkpodmod.sh
+++ doc/pod/mkpodmod.sh
@@ -1,16 +1,27 @@
 
 dir=$(realpath $(dirname "$0"))
-subdir=$(dirname $1)
-docdir="$dir/$subdir"
-modpath="$dir/../../source/lib/StreamGraph/$1.pm"
+sourcepath="$dir/../../source/lib/"
 
-if [ -e "$modpath" ]; then
-	cd $dir
-	mkdir -p $dir/$subdir
-	pod2texi --unnumbered-sections "$modpath" > "$1.texi" || echo "Error creating TexInfo"
-	sed -i"" '/@node Top/d' "$1.texi"
-	sed -i"" '/@top StreamGraph::.*/d' "$1.texi"
-	texi2pdf "$1.texi" -o "$1.pdf" &> /dev/null || echo "Error creating PDF"
+if [[ $# -eq 0 ]]; then
+	for pm in $(find $sourcepath -name "*.pm") ; do
+		pm=${pm#$sourcepath}
+		pm=${pm%.pm}
+		$dir/mkpodmod.sh $pm
+	done
 else
-	echo No module $modpath!
+	subdir=$(dirname $1)
+	docdir="$dir/$subdir"
+	modpath="$sourcepath/$1.pm"
+
+	if [ -e "$modpath" ]; then
+		cd $dir
+		mkdir -p $dir/$subdir
+		pod2texi --unnumbered-sections "$modpath" > "$1.texi" || echo "Error creating TexInfo"
+		sed -i"" '/@node Top/d' "$1.texi"
+		sed -i"" '/@top StreamGraph::.*/d' "$1.texi"
+		texi2pdf "$1.texi" -o "$1.pdf" &> /dev/null || echo "Error creating PDF"
+		echo "$1.pdf"
+	else
+		echo No module $modpath!
+	fi
 fi
